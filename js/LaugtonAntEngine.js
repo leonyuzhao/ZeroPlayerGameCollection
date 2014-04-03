f028ac0d5107552ec0f8bff0d218d5b61f26f1ef
Leon Yu Zhao <sephirothyzoe@gmail.com>
2014-04-03 20:47:58 +0100

    Langton's Ant Game

--- js/LaugtonAntEngine.js
+++ js/LaugtonAntEngine.js
@@ -1,82 +1,84 @@
-﻿function LangtonAntEngine(size, prefix, mark) {
-  this.size = size;
-  this.prefix = prefix;
-  this.mark = mark;
+﻿function LangtonAntEngine(boardSize, idPrefix, solidClassName) {
+    this.boardSize = boardSize;
+    this.idPrefix = idPrefix;
+    this.solidClassName = solidClassName;
 
-  var totalCellNum = size * size;
-  this.currentPos = Math.floor(Math.random() * totalCellNum);
+    var totalCellNum = boardSize * boardSize;
+    this.currentPos = Math.floor(Math.random() * totalCellNum);
 
-  // 0: Up, 1: Down, 2: Left, 3: Right
-  this.currentDirect = Math.floor(Math.random() * 3);
+    // 0: Up, 1: Down, 2: Left, 3: Right
+    this.currentDirect = Math.floor(Math.random() * 3);
 }
 
-LangtonAntEngine.prototype.TurnUp = function() {
-  var rowPos = Math.floor(this.currentPos / this.size);
-  if (rowPos > 0) {
-    this.currentPos = this.currentPos - this.size;
-    this.currentDirect = 0;
-  }
-};
-
-LangtonAntEngine.prototype.TurnDown = function() {
-  var rowPos = Math.floor(this.currentPos / this.size);
-  if (rowPos < this.size - 1) {
-    this.currentPos = this.currentPos + this.size;
-    this.currentDirect = 1;
-  }
-};
-
-LangtonAntEngine.prototype.TurnLeft = function() {
-  var colPos = this.currentPos % this.size;
-  if (colPos > 0) {
-    this.currentPos = this.currentPos - 1;
-    this.currentDirect = 2;
-  }
-};
-
-LangtonAntEngine.prototype.TurnRight = function() {
-  var colPos = this.currentPos % this.size;
-  if (colPos < this.size - 1) {
-    this.currentPos = this.currentPos + 1;
-    this.currentDirect = 3;
-  }
-};
-
-LangtonAntEngine.prototype.Move = function() {
-  if ($("#" + this.prefix + this.currentPos).hasClass(this.mark)) {
-    $("#" + this.prefix + this.currentPos).removeClass(this.mark);
-    // Black Square
-    switch (this.currentDirect) {
-      case 0:
-        this.TurnLeft();
-        break;
-      case 1:
-        this.TurnRight();
-        break;
-      case 2:
-        this.TurnDown();
-        break;
-      case 3:
-        this.TurnUp();
-        break;
+LangtonAntEngine.prototype.GoUp = function () {
+    var rowPos = Math.floor(this.currentPos / this.boardSize);
+    if (rowPos > 0) {
+        this.currentDirect = 0;
+        this.currentPos = this.currentPos - this.boardSize;
     }
-  }
-  else {
-    $("#" + this.prefix + this.currentPos).addClass(this.mark);
-    // White Square
-    switch (this.currentDirect) {
-      case 0:
-        this.TurnRight();
-        break;
-      case 1:
-        this.TurnLeft();
-        break;
-      case 2:
-        this.TurnUp();
-        break;
-      case 3:
-        this.TurnDown();
-        break;
+};
+
+LangtonAntEngine.prototype.GoDown = function () {
+    var rowPos = Math.floor(this.currentPos / this.boardSize);
+    if (rowPos < this.boardSize - 1) {
+        this.currentDirect = 1;
+        this.currentPos = this.currentPos + this.boardSize;
     }
-  }
-}; 
+};
+
+LangtonAntEngine.prototype.GoLeft = function () {
+    var colPos = this.currentPos % this.boardSize;
+    if (colPos > 0) {
+        this.currentDirect = 2;
+        this.currentPos = this.currentPos - 1;
+    }
+};
+
+LangtonAntEngine.prototype.GoRight = function () {
+    var colPos = this.currentPos % this.boardSize;
+    if (colPos < this.boardSize - 1) {
+        this.currentDirect = 3;
+        this.currentPos = this.currentPos + 1;
+    }
+};
+
+LangtonAntEngine.prototype.NextStep = function () {
+    // At a white square, turn 90° right, flip the color of the square, move forward one unit
+    // At a black square, turn 90° left, flip the color of the square, move forward one unit
+    if ($("#" + this.idPrefix + this.currentPos).hasClass(this.solidClassName)) {
+        $("#" + this.idPrefix + this.currentPos).removeClass(this.solidClassName);
+        // Black Square
+        switch (this.currentDirect) {
+            case 0:
+                this.GoLeft();
+                break;
+            case 1:
+                this.GoRight();
+                break;
+            case 2:
+                this.GoDown();
+                break;
+            case 3:
+                this.GoUp();
+                break;
+        }
+    }
+    else {
+        $("#" + this.idPrefix + this.currentPos).addClass(this.solidClassName);
+        // White Square
+        switch (this.currentDirect) {
+            case 0:
+                this.GoRight();
+                break;
+            case 1:
+                this.GoLeft();
+                break;
+            case 2:
+                this.GoUp();
+                break;
+            case 3:
+                this.GoDown();
+                break;
+        }
+    }
+};
